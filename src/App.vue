7f601cce9781baf08f48f40a535d5efc8477e083
Eugene Axe <siniypetr@gmail.com>
2021-12-03 15:53:12 +0300

    add metric

--- src/App.vue
+++ src/App.vue
@@ -77,7 +77,7 @@ export default {
   },
   data() {
     return {
-      navigationChecked: INFINITE_SCROLL,
+      navigationChecked: PAGINATION,
       dataComments: [],
       partData: [],
       scrollEnd: false,
